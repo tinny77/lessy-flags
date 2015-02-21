4d2cabeb63af5794fcbaadccbc57a2c35a4f9316
Daniel Thomas <danthms@outlook.com>
2015-02-21 01:38:38 +0000

    A - v1.0

---
+++ config.rb
@@ -0,0 +1,8 @@
+require 'compass/import-once/activate'
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+output_style = :nested
+line_comments = false
+color_output = false
+preferred_syntax = :scss
---
+++ example.php
@@ -0,0 +1,28 @@
+<!--
+
+//	SassyFlags v1.0
+//	Made by Daniel Thomas
+
+//	Your rights
+		Do what ever you want, if you do something
+		cool then let me know @_dthms (twitter)
+
+-->
+
+<html>
+<head><link href='sassy-flags.css' rel='stylesheet'></head>
+<body>
+
+<!--
+
+1.	You have to add the class of "flag".
+2.	Then also add the country code of the flag you want.
+3.	Done!
+
+-->
+
+<!-- THIS IS THE EXAMPLE -->
+<span class="flag us"></span>
+
+</body>
+</html>
